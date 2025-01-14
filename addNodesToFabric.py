ef0fea058f511674f899e83d2bd3f522e8674573
Chris Paggen <cpaggen@cisco.com>
2025-01-14 02:07:07 +0100

    add nodes script v4

--- addNodesToFabric.py
+++ addNodesToFabric.py
@@ -26,16 +26,16 @@ def deleteFabNodes(fabName):
         delete = requests.request('DELETE', endpoint, headers=headers, verify=True) 
         print(delete.text)
 
-def genPayload(numDevices):
+def genPayload(numDevices,devRole):
     nodeDict = {}
     nodes = []
     for i in range(numDevices):
         node = {
-         "name": f"node-leaf{i}",
+         "name": f"node-{devRole}{i}",
          "description": f"Example node {i}",
          "enabled": True,
          "serialNumber": f"SERIAL_{i}",
-         "roles": ["LEAF"],
+         "roles": [f"{devRole.upper()}"],
          "labels": [f"LABEL_{random.randint(1, 5)}"],
          "modelName": "HF6100-32D"
          }
@@ -49,16 +49,18 @@ def pushChanges(endpoint,payload):
     fabric = response.json()
     print(f"Response ==> {json.dumps(fabric)}")
 
-def main(fabName,numDevices):
-    nodes = deleteFabNodes(fabName)
-    payload = genPayload(numDevices)
+def main(fabName,numDevices,devRole):
+    if numDevices == 0:
+        deleteFabNodes(fabName)
+    payload = genPayload(numDevices,devRole)
     url = f"https://hyperfabric.cisco.com/api/v1/fabrics/{fabName}/nodes"
     pushChanges(url,payload)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit(f"Usage {sys.argv[0]} <fabName> <numDevices>")
-    numDevices = int(sys.argv[2])
+    if len(sys.argv) != 4:
+        sys.exit(f"Usage {sys.argv[0]} <fabName> <numDevices [0 deletes all nodes]> <devRole>")
     fabName = sys.argv[1]
-    main(fabName,numDevices)
+    numDevices = int(sys.argv[2])
+    devRole = sys.argv[3]
+    main(fabName,numDevices,devRole)
 
