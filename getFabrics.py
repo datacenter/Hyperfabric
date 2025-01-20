face79ce2183435e587a6ad946ed422edfba1a1b
Chris Paggen <cpaggen@cisco.com>
2025-01-20 16:46:26 +0100

    getFabrics

--- getFabrics.py
+++ getFabrics.py
@@ -2,7 +2,6 @@ import requests
 import os
 from pprint import pprint
 
-url = "https://hyperfabric.cisco.com/api/v1/fabrics"
 token = os.environ['AUTH_TOKEN']
 
 headers = {
@@ -11,14 +10,14 @@ headers = {
   "Authorization": "Bearer " + token,
 }
 
-payload = None
-headers = {
-  "Content-Type": "application/json",
-  "Accept": "application/json",
-  "Authorization": "Bearer " + token,
-}
+def main():
+    url = "https://hyperfabric.cisco.com/api/v1/fabrics"
+    response = requests.request('GET', url, headers=headers)
+    fabrics = response.json()
+    for fabric in fabrics['fabrics']:
+        pprint(fabric)
+        print(80*"=")
 
-response = requests.request('GET', url, headers=headers, data = payload)
-pprint(response.json())
-
+if __name__ == "__main__":
+    main()
 
